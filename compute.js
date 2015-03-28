edc644a24b3dc24595f19702d03b02167260f583
GamingCoder <coreoder@gmail.com>
2015-03-28 17:05:55 +0100

    Add compute.js and test it

---
+++ compute.js
@@ -0,0 +1,29 @@
+module.exports = function(conf) {
+
+	function containsObjects (c) {
+		var contains = false;
+		for (e in c){
+			if (typeof c[e] === 'object') contains = true;
+		}
+		return contains;
+	}
+
+	while(containsObjects(conf)) {
+		for(name in conf) {
+			var val = conf[name];
+			if (typeof val === 'object') {
+				if (val.active === false) {
+					conf[name] = false
+				} else {
+					depsActive = true;
+					for (dep in val.required){
+						if (conf[val.required[dep]] !== true) depsActive = false;
+					}
+					conf[name] = depsActive;
+				}
+			}
+		}
+	}
+
+	return conf;
+}
---
+++ test/compute.js
@@ -0,0 +1,64 @@
+var expect = require('expect.js');
+var compute = require('../compute');
+
+describe('compute.js', function() {
+	it('should export method', function() {
+		expect(typeof compute).to.eql('function');
+	});
+	it('should return object', function() {
+		conf = {}
+		expect(typeof compute(conf)).to.eql('object');
+	});
+	it('should set active features to true', function() {
+		conf = {
+			"feature_a": true,
+			"feature_b": false
+		}
+		expected = {
+			"feature_a": true,
+			"feature_b": false
+		}
+		expect(compute(conf)).to.eql(expected);
+	});
+	it('should only activate features only when required features are active', function() {
+		conf = {
+			"feature_a": true,
+			"feature_b": false,
+			"true_dep_a": {
+				"active": true,
+				"required": ['feature_a']
+			},
+			"false_dep_a": {
+				"active": false,
+				"required": ['feature_a']
+			},
+			"true_dep_b": {
+				"active": true,
+				"required": ['feature_b']
+			},
+			"false_dep_b": {
+				"active": false,
+				"required": ['feature_b']
+			},
+			"true_dep_ab": {
+				"active": true,
+				"required": ['feature_a', 'feature_b']
+			},
+			"false_dep_ab": {
+				"active": false,
+				"required": ['feature_a', 'feature_b']
+			}
+		}
+		expected = {
+			"feature_a": true,
+			"feature_b": false,
+			"true_dep_a": true,
+			"false_dep_a": false,
+			"true_dep_b": false,
+			"false_dep_b": false,
+			"true_dep_ab": false,
+			"false_dep_ab": false
+		}
+		expect(compute(conf)).to.eql(expected);
+	});
+});
